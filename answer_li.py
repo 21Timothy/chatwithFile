f969928a2e3f59dda68872376385ad48fe9b9edd
gjqg4 <gjqg4@tencent.com>
2023-04-18 10:23:47 +0800

    update

--- answer_li.py
+++ answer_li.py
@@ -2,6 +2,8 @@ from flask import Flask, request, jsonify, g, render_template
 from flask_cors import CORS
 import openai
 from langchain.document_loaders.unstructured import UnstructuredFileLoader
+from langchain.document_loaders.image import UnstructuredImageLoader
+from langchain.document_loaders.csv_loader import CSVLoader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
@@ -29,17 +31,17 @@ def upload():
         file.save(save_path)
         extension = os.path.splitext(file.filename)[1]
         if extension in [".csv", ".xls", ".xlsx"]:
-
-        loader = UnstructuredFileLoader(save_path)
+            loader = CSVLoader(save_path)
+        elif extension in [".jpg", "jpeg", "png"]:
+            loader = UnstructuredImageLoader(save_path)
+        else:
+            loader = UnstructuredFileLoader(save_path)
         documents = loader.load()
         text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
         texts = text_splitter.split_documents(documents)
         embeddings = OpenAIEmbeddings()
         db = Chroma.from_documents(texts, embeddings)
-        retriever = db.as_retriever(search_type="similarity", search_kwargs={"k":1})
-        # qa = RetrievalQA.from_chain_type(llm=OpenAI(), chain_type="stuff", retriever=retriever,
-        #                                  return_source_documents=True)
-        qa = ConversationalRetrievalChain.from_llm(OpenAI(temperature=0), retriever)
+        retriever = db.as_retriever(search_type="similarity", search_kwargs={"k":10})
         return jsonify({'answer': '上传成功'})
     except Exception as e:
         print(str(e))
